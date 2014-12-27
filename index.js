6e38bcf6f21f29cd3fa56ec6a595b68c68d9fe02
Guilherme Souza <sitegui@sitegui.com.br>
2014-12-27 02:44:57 -0200

    Added support for websocket

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict'
 
 module.exports = require('./lib/Context')
+
+// Set maxBufferLength for WebSocket connections as 17MiB
+// This give enough space for the maximum frame size (16MiB)
+require('nodejs-websocket').setMaxBufferLength(17 * 1024 * 1024)
